605bae7a056d0ef9be4a647a886dcd2fa7768487
Henrique Fernandes <fernandeshenrique15@gmail.com>
2018-06-21 18:00:40 -0300

    Boostrap 4 Menu

--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -2,34 +2,38 @@
 	<head>
 	    <!-- <link href="/css/app.css" rel="stylesheet"> -->
 	    <link href="/css/custom.css" rel="stylesheet">
-	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+	    <!-- Icon Google -->
+	    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 	    <script src="{{ asset('js/app.js') }}" defer></script>
     	<link rel="dns-prefetch" href="https://fonts.gstatic.com">
     	<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 	    <title>Controle de Horas</title>
 	</head>
 	<body>
-		<nav class="navbar navbar-default">
-		  <div class="container">
+		<nav class="navbar navbar-expand-md navbar-dark bg-dark nav-pills nav-fill">
+		    <div class="container">
 
 		  <div class="navbar-header">
 		    <a class="navbar-brand" href="{{action('RecordController@lista')}}">Controle de Horas</a>
 		  </div>
 
-		   <ul class="nav navbar-nav navbar-right">
+		   <ul class="navbar-nav">
 		   	@guest
-                  <li><a class="nav-link" href="{{ route('login') }}">{{ __('Entrar') }}</a></li>
+                  <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">{{ __('Entrar') }}</a></li>
               @else
-                <li {{{ (Request::is(' ') ? 'class=active' : '') }}} class="nav-item">
-				      <a class="nav-link" href="{{action('RecordController@lista')}}">
+                <li class="nav-item">
+				      <a class="nav-link {{{ (Request::is('/') ? 'active' : '') }}}" href="{{action('RecordController@lista')}}">
 				          Registros
 				      </a>
 				  </li>
-		        <li {{{ (Request::is('work') ? 'class=active' : '') }}}><a href="{{action('WorkController@lista')}}">Funcionários</a></li>
-		        <li {{{ (Request::is('department') ? 'class=active' : '') }}}><a href="{{action('DepartmentController@lista')}}">Departamentos</a></li>
-		        <li {{{ (Request::is('user') ? 'class=active' : '') }}}><a href="{{action('UserController@lista')}}">Administradores</a></li>
+		        <li class="nav-item"><a href="{{action('WorkController@lista')}}" class="nav-link {{{ (Request::is('work') ? 'active' : '') }}}">Funcionários</a></li>
+		        <li class="nav-item"><a href="{{action('DepartmentController@lista')}}" class="nav-link {{{ (Request::is('department') ? 'active' : '') }}}">Departamentos</a></li>
+		        <li class="nav-item"><a href="{{action('UserController@lista')}}" class="nav-link {{{ (Request::is('user') ? 'active' : '') }}}">Administradores</a></li>
                   <li class="nav-item dropdown">
-                      <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                      <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" >
                           {{ Auth::user()->name }} <span class="caret"></span>
                       </a>
 
@@ -63,5 +67,10 @@
 		    	<p>© Direitos.</p>
 		    </footer>
 	  	</div>
+
+	  	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+	    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 	</body>
 </html>
